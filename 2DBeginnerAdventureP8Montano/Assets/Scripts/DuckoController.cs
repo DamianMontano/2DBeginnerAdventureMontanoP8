8631116d5c7c03bc680f70cf751b472054e63eee
DamianMontano <dee3montano@gmail.com>
2024-11-20 15:23:36 -0800

    added stuff to my ducko script.

--- 2DBeginnerAdventureP8Montano/Assets/Scripts/DuckoController.cs
+++ 2DBeginnerAdventureP8Montano/Assets/Scripts/DuckoController.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class DuckoController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("vertical");
+       
+        
 
         Vector2 position = transform.position;
         position.x = position.x + 5.0f * horizontal * Time.deltaTime;
